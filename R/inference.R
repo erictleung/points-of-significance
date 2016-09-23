b1cf3c8d3167a64d3730cc3c7be5f258bc2fe455
Eric Leung <eric@erictleung.com>
2016-09-22 22:47:55 -0700

    inference.R: function to find inference errors

--- R/inference.R
+++ R/inference.R
@@ -18,6 +18,9 @@
 #' @export
 #'
 #' @examples
-inference <- function(alpha, beta) {
+#' inference(alpha = 0.05, beta = 0.2)
+#' inference(alpha = 0.001, beta = 0.2)
+#' inference(alpha = 0.05, beta = 0.5)
+inference <- function(alpha = 0.05, beta = 0.2) {
     list(specificity = 1 - alpha, sensitivity = 1 - beta)
 }
