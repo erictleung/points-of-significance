70a046adacb6eef701399544b12c30d22ffc94db
Eric Leung <eric@erictleung.com>
2016-09-22 18:33:47 -0700

    create-boxplots.R: create and compile documentation

--- R/create-boxplots.R
+++ R/create-boxplots.R
@@ -1,3 +1,14 @@
+#' Create Histogram and Box Plot
+#'
+#' @param n sample size for box plots
+#' @param s1,s2 non-negative parameters of the Beta distribution
+#'
+#' @return histrogram, summary statistics of data, and box plot
+#'
+#' @examples
+#' create_boxplots()
+#' create_boxplots(n = 100)
+#' create_boxplots(n = 100, s1 = 5, s2 = 2)
 create_boxplots <- function(n = 1000, s1 = 2, s2 = 5) {
     par(mfrow = c(2, 1))
 
