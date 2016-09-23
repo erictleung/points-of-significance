9ff3ef2d545e3afd42fd6f78a429e8fd459bd359
Eric Leung <eric@erictleung.com>
2016-09-22 22:33:21 -0700

    inference.R: calculates inference errors

---
+++ R/inference.R
@@ -0,0 +1,23 @@
+#' Inference Errors
+#'
+#' Returns the specificity and sensitivity values for a given alpha and beta
+#' value.
+#'
+#' Specificity is also known as the true negative rate, which is calculated as
+#' the number of true negatives divided by the sum of true negatives and false
+#' positives.
+#'
+#' Sensitivity is also known as the true positive rate, which is calculated as
+#' the number of true positives divided by the sum of true positives and false
+#' negatives.
+#'
+#' @param alpha Type I error or the false positive error
+#' @param beta Type II error or the false negative error
+#'
+#' @return list containing the specificity and sensitivity
+#' @export
+#'
+#' @examples
+inference <- function(alpha, beta) {
+    list(specificity = 1 - alpha, sensitivity = 1 - beta)
+}
