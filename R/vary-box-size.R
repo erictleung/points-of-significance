395df5d25fe9092121de77f5e5ddbbec0e4f5c2a
Eric Leung <eric@erictleung.com>
2016-09-26 02:15:32 -0700

    vary-box-size(): add documentation

--- R/vary-box-size.R
+++ R/vary-box-size.R
@@ -1,11 +1,37 @@
-vary_box_size <- function(n = 5) {
-    # Number of different plots
-    num <- 5
+#' Vary Box Plot Sizes
+#'
+#' Plots \code{plotNum} number of box plots with the same sample size \code{n}
+#' sampled from a normal distribution.
+#'
+#' Box plots are very powerful visualization tools. However, you need sufficient
+#' number of samples for them to be confident sources of information.
+#'
+#' Try changing the sample size of the plots drawn to understand the variability
+#' in visualizing small datasets.
+#'
+#' @param n sample size
+#' @param plotNum number of box plots
+#'
+#' @return Plot box plots of five samples from a normal distribution
+#'
+#' @references Krzywinski, Martin, and Naomi Altman. "Points of significance:
+#'   visualizing samples with box plots." Nature Methods 11.2 (2014): 119-120.
+#'
+#' @examples
+#' vary_box_size()
+vary_box_size <- function(n = 5, plotNum = 5) {
+    if (plotNum < 0) {
+        stop("The argument `plotNum` needs to be positive")
+    }
+    if (n < 0) {
+        stop("The argument `n` needs to be positive")
+    }
 
     # Make empty data frame
     mat <- data.frame(data = NULL, plot = NULL)
 
-    for (i in 1:num) {
+    # Create data to plot
+    for (i in 1:plotNum) {
         vals <- rnorm(n)
         temp <- cbind(vals, plot = i)
         mat <- rbind(mat, temp)
